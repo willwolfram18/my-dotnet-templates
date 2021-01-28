b0e1bbd3bc6f78326da65b8c005b1264536d0e17
William Wolfram <willwolfram18@gmail.com>
2021-01-27 22:07:32 -0800

    Initial commit of all project files
    Commit containing all of the project files and solution

---
+++ src/willwolfram18.DotNetTemplates.Web/templates/clean-rest-api/test/Infrastructure.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Infrastructure.Tests.Unit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
